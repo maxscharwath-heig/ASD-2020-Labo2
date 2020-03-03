3b9346ff8d71effd96c7ceba5a1e5e457343b7a8
soso24soso <61196412+soso24soso@users.noreply.github.com>
2020-03-03 16:04:33 +0100

    début

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "pieces.h"
+#include <array>
+
+using namespace std;
+
+struct P{
+    size_t id;
+    size_t rot;
+};
+
+using Tableau = array<P,9>;
+
+void solutionRec(P pi, size_t pos, Tableau tab);
+
+bool tester(P pi, const Tableau& tab);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
+}
+
+void solutionRec(P pi, size_t pos, Tableau tab){
+
+
 }
