a20edb8ed59815a3bca723166bc7e13bb5e94c04
Klab93 <37708074+Klab93@users.noreply.github.com>
2020-03-15 13:23:43 +0100

    Merge branch 'master' of D:\Documents\01_heig\02_ASD\labo\HEIG_ASD_LABO2 with conflicts.

--- Kylian.cpp
+++ Kylian.cpp
@@ -8,38 +8,84 @@
 using namespace std;
 
 struct P {
-    size_t id  = 0;
+    P(size_t id = 0, size_t rot = 0) : id(id), rot(rot) {}
+
+    size_t id;
     size_t rot = 0;
+
+    Piece getPiece() const {
+        return PIECES[id];
+    }
+
+    string getCode() {
+        char v = 'a' + rot;
+        return to_string(id + 1) + v;
+    }
 };
 
-using Tableau = array<size_t , 3>;
+using Tableau = array<P, 9>;
 
 void solutionRec(Tableau& tab, size_t taille);
 
 //void solutionRec(size_t idPi, size_t pos, Tableau &tab);
 
-//bool tester(size_t p1, size_t p2, const Tableau &tab);
+bool tester(size_t p1, size_t p2, const Tableau &tab);
 
 int main() {
-    Tableau tab = {1,3,5};
+    Tableau tab = {P(0), P(1), P(2), P(3), P(4), P(5), P(6), P(7), P(8)};
     solutionRec(tab, tab.size() );
     //solutionRec(8, 0, tab);
     return 0;
 }
 
+bool checkAttachement(AttachementType aA, AttachementType aB) {
+    switch (aA) {
+        case DAME_HAUT:
+            return aB == DAME_BAS;
+        case DAME_BAS:
+            return aB == DAME_HAUT;
+        case FILLE_HAUT:
+            return aB == FILLE_BAS;
+        case FILLE_BAS:
+            return aB == FILLE_HAUT;
+        case ARROSOIR_GAUCHE:
+            return aB == ARROSOIR_DROIT;
+        case ARROSOIR_DROIT:
+            return aB == ARROSOIR_GAUCHE;
+        case GATEAU_GAUCHE:
+            return aB == GATEAU_DROIT;
+        case GATEAU_DROIT:
+            return aB == GATEAU_GAUCHE;
+        default:
+            return false;
+    }
+}
 
+bool tester(size_t p, const Tableau &tab) {
+    if (p - 3 > 0) {
+        if (!checkAttachement(tab[p].getPiece()[0], tab[p - 3].getPiece()[2]))
+            return false;
+    }
+    if (p - 1 > 0) {
+        if (!checkAttachement(tab[p].getPiece()[3], tab[p - 1].getPiece()[1]))
+            return false;
+    }
+    return true;
+}
 
 void solutionRec(Tableau& tab, size_t taille ){
     if( taille == 1 ){
         for(size_t i = 0; i < 3; ++i)
-            cout << tab[i];
+            cout << tab[i].id;
         cout << endl;
     }else{
         for(size_t i = 0; i < taille - 1; ++i ){
-
-            swap(tab[i], tab[taille - 1]);
-            solutionRec( tab, taille-1);
-            swap(tab[i], tab[taille - 1]);
+            for(size_t j = 0; j < 4; ++j)
+                if( tester(taille,tab) ){
+                    swap(tab[i], tab[taille - 1]);
+                    solutionRec( tab, taille-1);
+                    swap(tab[i], tab[taille - 1]);
+                }
         }
         solutionRec(tab, taille-1);
     }
--- Maxime.cpp
+++ Maxime.cpp
@@ -4,62 +4,11 @@
 
 using namespace std;
 
-struct P {
-   P(size_t id = 0, size_t rot = 0) : id(id), rot(rot) {}
 
-   size_t id = 0;
-   size_t rot = 0;
-
-   Piece getPiece() const {
-      return PIECES[id];
-   }
-
-   AttachementType getAttachement(size_t r = 0) {
-      getPiece()[(rot + r) % 4]
-   }
-
-   string getCode() {
-      char v = 'a' + rot;
-      return to_string(id + 1) + v;
-   }
-};
 
 using Tableau = array<P, 9>;
 
-bool checkAttachement(AttachementType aA, AttachementType aB) {
-   switch (aA) {
-      case DAME_HAUT:
-         return aB == DAME_BAS;
-      case DAME_BAS:
-         return aB == DAME_HAUT;
-      case FILLE_HAUT:
-         return aB == FILLE_BAS;
-      case FILLE_BAS:
-         return aB == FILLE_HAUT;
-      case ARROSOIR_GAUCHE:
-         return aB == ARROSOIR_DROIT;
-      case ARROSOIR_DROIT:
-         return aB == ARROSOIR_GAUCHE;
-      case GATEAU_GAUCHE:
-         return aB == GATEAU_DROIT;
-      case GATEAU_DROIT:
-         return aB == GATEAU_GAUCHE;
-      default:
-         return false;
-   }
-}
 
-bool tester(size_t p, const Tableau &tab) {
-   if (p - 3 > 0) {
-      if (!checkAttachement(tab[p].getAttachement(0), tab[p - 3].getAttachement(2)))
-         return false;
-   }
-   if (p - 1 > 0) {
-      if (!checkAttachement(tab[p].getAttachement(3), tab[p - 1].getAttachement(1)))
-         return false;
-   }
-   return true;
-}
 
 void solutionRec(size_t idPi, size_t pos, Tableau &tab) {
    // PIECES[idPi]
